06ed39777ebd720ec2a30f61348b59b9467191ff
limhansol <limhs@asianaidt.com>
2022-10-21 17:13:14 +0900

    초기 커밋.

---
+++ LogAnal/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace LogAnal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(405, 88);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "분석시작";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.richTextBox1, 2);
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(3, 97);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(817, 488);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(100, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ON_결과";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(159, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(126, 21);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(95, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "OFF_결과";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(159, 51);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(126, 21);
+            this.textBox2.TabIndex = 6;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.richTextBox1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.15646F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 83.84354F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(823, 588);
+            this.tableLayoutPanel1.TabIndex = 7;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(414, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(406, 88);
+            this.panel1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(823, 588);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ LogAnal/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LogAnal
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, int> sensor_ON_real = new Dictionary<string, int>();
+        Dictionary<string, int> sensor_OFF_real = new Dictionary<string, int>();
+        Dictionary<string, int> sensor_ON_test = new Dictionary<string, int>();
+        Dictionary<string, int> sensor_OFF_test = new Dictionary<string, int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path_real = @"C:\Users\USER\Desktop\새 폴더 (7)\Real_Log.txt";
+            LogToDictionary(path_real, sensor_ON_real, sensor_OFF_real);
+            string path_test = @"C:\Users\USER\Desktop\새 폴더 (7)\Test_Log.txt";
+            LogToDictionary(path_test, sensor_ON_test, sensor_OFF_test);
+                      
+            foreach(string s in sensor_ON_real.Keys)
+            {
+                if(sensor_ON_test.ContainsKey(s))
+                {
+                    if(sensor_ON_real[s] != sensor_ON_test[s])
+                    {
+                        textBox1.Text = "불일치";
+                        richTextBox1.AppendText($"MAC address : {s}  //  realLog : {sensor_ON_real[s]}  //  testLog : {sensor_ON_test[s]}  //  [ON]");
+                        richTextBox1.AppendText("\r\n");
+                    }
+                        
+                }
+                else
+                {
+                    textBox1.Text = "불일치";
+                    richTextBox1.AppendText($"{s}가 불일치");
+                    richTextBox1.AppendText("\r\n");
+                }               
+            }
+            if (textBox1.Text != "불일치")
+                textBox1.Text = "일치";
+
+            foreach (string s in sensor_OFF_real.Keys)
+            {
+                if (sensor_OFF_test.ContainsKey(s))
+                {
+                    if (sensor_OFF_real[s] != sensor_OFF_test[s])
+                    {
+                        textBox2.Text = "불일치";
+                        richTextBox1.AppendText($"MAC address : {s}  //  realLog : {sensor_OFF_real[s]}  //  testLog : {sensor_OFF_test[s]}  //  [OFF]");
+                        richTextBox1.AppendText("\r\n");
+                    }
+
+                }
+                else
+                {
+                    textBox2.Text = "불일치";
+                    richTextBox1.AppendText($"{s}가 불일치");
+                    richTextBox1.AppendText("\r\n");
+                }
+            }
+            if (textBox2.Text != "불일치")
+                textBox2.Text = "일치";
+        }
+
+        public static string LoadFile(string FilePath)
+        {
+            using (FileStream fs = new FileStream(FilePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                using (StreamReader sr = new StreamReader(fs, Encoding.UTF8))
+                {
+                    return sr.ReadToEnd();
+                }
+            }
+        }
+
+        private void LogToDictionary(string filePath, Dictionary<string, int> sensor_on, Dictionary<string, int> sensor_off)
+        {
+            string totalSensorLog = LoadFile(filePath);
+            string[] sensorLogArray = totalSensorLog.Split('\n');
+            List<string> undatedSensorArray = new List<string>();
+            foreach (string sensorLog in sensorLogArray)
+            {
+                if (sensorLog.Contains("[UpdateSensor]"))
+                {
+                    //[UpdateSensor]가 포함된 로그 한 줄 List추가
+                    undatedSensorArray.Add(sensorLog);
+
+                    // [UpdateSensor]의 센서 MAC
+                    string sensor_MAC = sensorLog.Substring(58, 12);
+
+                    // true와 false여부에 따른 두개의 딕셔너리에 추가하는 구문
+                    if (sensorLog[71] == 'T')
+                    {
+                        if (!sensor_on.ContainsKey(sensor_MAC))
+                        {
+                            sensor_on.Add(sensor_MAC, 1);
+                        }
+                        else
+                        {
+                            sensor_on[sensor_MAC]++;
+                        }
+                    }
+                    else
+                    {
+                        if (!sensor_off.ContainsKey(sensor_MAC))
+                        {
+                            sensor_off.Add(sensor_MAC, 1);
+                        }
+                        else
+                        {
+                            sensor_off[sensor_MAC]++;
+                        }
+                    }
+
+                }
+            }         
+        }
+
+    }
+}
